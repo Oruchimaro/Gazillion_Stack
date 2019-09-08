86f3e34938cdb3d2e6863fc6ef8640ec6c5157a7
AmirDaneshkar <amir.daneshkar@hotmail.com>
2019-09-08 10:39:51 +0430

    adding vue-izitoast package to beautify the confrimationa nd notifications

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,21 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueIziToast from 'vue-izitoast';
+
+import 'izitoast/dist/css/iziToast.css';
+
+// import 'izitoast/dist/css/iziToast.min.css';
+
+Vue.use(VueIziToast);
+
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('user-info', require('./components/UserInfo.vue').default);
 Vue.component('answer', require('./components/Answer.vue').default);
+
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
